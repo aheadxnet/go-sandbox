8249e668a045a6170d0d13415fb729cfa7b914ed
Stefan <development@aheadx.net>
2022-02-20 11:54:50 +0100

    First custom HTTP handler returning JSON and XML with parameters

--- src/hello-world/main.go
+++ src/hello-world/main.go
@@ -1,13 +1,30 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/xml"
+	"github.com/gin-gonic/gin"
+)
+
+func IndexHandler(c *gin.Context) {
+	name := c.Params.ByName("name")
+	c.JSON(200, gin.H{
+		"message": "hello " + name,
+	})
+}
+
+type Person struct {
+	XMLName xml.Name `xml:"person"`
+	Name    string   `xml:"name,attr"`
+}
+
+func XmlHandler(c *gin.Context) {
+	name := c.Params.ByName("name")
+	c.XML(200, Person{Name: name})
+}
 
 func main() {
 	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "hello world",
-		})
-	})
+	router.GET("/hello/:name", IndexHandler)
+	router.GET("/xml/:name", XmlHandler)
 	router.Run()
 }
